2dca56640d134d0cee10411b7e4e087d455773a1
Ardons <alca.amb.96@gmail.com>
2025-04-28 21:50:24 -0500

    se cambia nuevamente urls y static

--- coffee_shop/coffee_shop/urls.py
+++ coffee_shop/coffee_shop/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path("ordenes/", include("orders.urls")),
 ]
 
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
