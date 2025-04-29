eb7a7ace43218b8d056d7475ba4e85f42be4cd20
Ardons <alca.amb.96@gmail.com>
2025-04-28 20:12:16 -0500

    prueba con url producst

--- coffee_shop/products/urls.py
+++ coffee_shop/products/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import ProductFormView, ProductsListView, ProductList2View, ProductListAPI
-
+from django.http import HttpResponse
 
 urlpatterns = [
-    path("", ProductList2View.as_view(), name="lista_productos_2"),
+    path("", lambda request: HttpResponse("OK - Fast Test!"), name="lista_productos_2"),
     path("agregar/", ProductFormView.as_view(), name="add_product"),
     path("lista_productos/", ProductsListView.as_view(), name="lista_productos"),    
     path("api/", ProductListAPI.as_view(), name="api"),
 ]
 
 #lista_productos_2/
+#lambda request: HttpResponse("OK - Fast Test!") path("", ProductList2View.as_view(), name="lista_productos_2")
